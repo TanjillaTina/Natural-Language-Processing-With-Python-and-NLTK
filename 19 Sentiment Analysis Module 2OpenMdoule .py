76392f6d70b80ef3d9701a668d93eab8a0d82b28
TanjillaTina <tanjilla.tina@gmail.com>
2018-01-01 13:35:24 +0600

    SentimentAnalysisModules

---
+++ 19 Sentiment Analysis Module 2OpenMdoule .py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 18:00:22 2017
+In The name of Allah,The Beneficent and The Merciful
+
+@author: TINA
+"""
+
+import sentiment_mod as s
+
+print(s.sentiment("I loved the Fantastic Beasts and Where to Find Them movie.... Awesome screenplay, awesome grapchics.There is nothing bad, rubbish, stupid in this movie. Tina,Queenie,"))
